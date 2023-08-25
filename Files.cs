728a6830e7edde16eb5d77a838ee63cb87b6f5d4
floxiceeq <116076818+Floxen@users.noreply.github.com>
2023-08-25 02:21:36 +0200

    Add files via upload

---
+++ Files.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCLogger
+{
+    internal class Files
+    {
+        public static long getFilesize(string pathToFile)
+        {
+            return new FileInfo(pathToFile).Length;
+        }
+
+        public static string getLastFileLine(string pathToFile)
+        {
+            if (!File.Exists(pathToFile)) return null; return File.ReadLines(pathToFile).Last();
+        }
+    }
+}
